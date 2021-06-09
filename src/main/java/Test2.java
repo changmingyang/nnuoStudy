7c230e647a29cc8e1f897da6623cd96cf8c44fc7
CHANGMINGYANG\admin <cmy829514@163.com>
2021-06-09 17:29:39 +0800

    testMaster

---
+++ src/main/java/Test2.java
@@ -0,0 +1,30 @@
+/**
+ * 描述：测试类
+ * 主要用来返回int类型数字相加结果
+ * @author Changmingyang
+ * @date 2021/6/9 17:22
+ */
+public class Test2 {
+
+    /**
+     * 打印相加值
+     * @param a
+     * @param b
+     */
+    public void add(int a, int b){
+        System.out.println(a + b);
+    }
+
+    /**
+     * 返回三个数相加值
+     * @param a
+     * @param b
+     * @param c
+     * @return
+     */
+    public int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+
+}
