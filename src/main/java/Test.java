04100032de6c965aa5cf6c5e99cecf4bf949e3b8
CHANGMINGYANG\admin <cmy829514@163.com>
2021-06-09 16:07:27 +0800

    testStudy

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 描述：
+ *
+ * @author Chenjia
+ * @date 2021/6/9 15:46
+ */
+public class Test {
+    public static void main(String [] args){
+        System.out.println("Test");
+    }
+
+}
